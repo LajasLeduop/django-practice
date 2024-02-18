3954c384e274ff85cd76f2972300f15f21babd8d
Sajal <sajalpoudel73@gmail.com>
2024-02-18 14:14:45 +0545

    changes to make sure the code works

--- webProject/somtuEvents/views.py
+++ webProject/somtuEvents/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
+from datetime import datetime
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
-    return HttpResponse("Welcome User!!!!")
+    now= datetime.now()
+    if now.month==1 and now.day==1:
+        message="Happy New Year!!!!"
+    else:
+        message="Sorry, It's not new year yet."
+
+    data={
+        "name": "Sajal Poudel",
+        "message":message
+    }
+    return render(request,"index.html",data)
